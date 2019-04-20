bb5a97475d96a507286313927513c4aaa30b25b8
Naoto Suzuki <crappy.naoto@gmail.com>
2019-04-21 02:34:32 +0900

    Genre.phpモデルのgenreクラスでテーブル名をセットしていなかったせいでエラー

--- app/models/Genre.php
+++ app/models/Genre.php
@@ -5,12 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
+
 class Genre extends Model
 {
+    protected $table = 'genres';
+    protected $guarded = array('id');
+    public $timestamps = false;
+
     public function getData(){
         $genre_data = DB::table($this->table)->get();
-
-        return $genre_data;
+    return $genre_data;
     }
 
 }
