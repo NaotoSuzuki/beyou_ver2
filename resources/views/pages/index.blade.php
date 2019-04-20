a39e21184c8675e2ffdb32370700593088e8cf39
Naoto Suzuki <crappy.naoto@gmail.com>
2019-04-21 03:34:52 +0900

    boostrap効かせられたのはいいけどレイアウト全体が左に寄った

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,10 +7,14 @@
   <a href="/" class="header-menu">Beyou</a>
   文法を選んでください。
 </h1>
-<ul>
-    @foreach($genres_data as $genre_data)
-        <li>{{$genre_data->genre}}</li>
-    @endforeach
-</ul>
+    <div class = "container">
+        <div class="row">
+            @foreach($genres_data as $genre_data)
+                <div class="col-sm-2">
+                <p>{{$genre_data->genre}}</p>
+                </div>
+            @endforeach
+        </div>
+    </div>
 <script src="/js/main.js"></script>
 @endsection
