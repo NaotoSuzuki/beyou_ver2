b51003e9a3837deca2d30e8a561818e47b1d54e1
Naoto Suzuki <crappy.naoto@gmail.com>
2019-04-20 09:31:32 +0900

    genresテーブルへのデータ挿入と、データ取得ロジックのあるモデルを作成

--- app/Genre.php
+++ app/Genre.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Genre extends Model
 {
-    //
+    public function getData(){
+        $genre_data = DB::table($this->table)->get();
+
+        return $genre_data;
+    }
+
 }
