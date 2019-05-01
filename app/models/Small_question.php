79fcf7d88e0dd70a9b0da7585c2ca9bf1629bfdd
unknown <crappy.naoto@gmail.com>
2019-05-01 11:28:26 +0900

    今まで作ってきたmodelをModel配下に移動させてみた

--- app/models/Small_question.php
+++ app/models/Small_question.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Small_question extends Model
 {
     //
+    protected $fillable = ['genre_value','big_question_id','question_num','question','answer'];
 }
