0f3800208e76cd6e53216643d2873de5f6c03246
unknown <crappy.naoto@gmail.com>
2019-05-11 00:46:29 +0900

    Namespace declaration statement has to be the very first statement in the script このエラーはコードのエンコーディング宣言の上に改行があるだけで発生する

---
+++ app/Helpers/BuildArraysHelper.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Helpers;
+//問題表示や回答保存用の配列を作るための処理を書いていく
+
+
+ 
+class ArrayBuilder
+{
+    public function indicateQuesstionsArray(){
+      return 'TestEchoHelperクラスの呼び出しが成功しました！';
+    }
+ }
