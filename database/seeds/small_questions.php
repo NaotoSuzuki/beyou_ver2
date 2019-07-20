5ec88ec96ff3c8d2a0b76a6d32df1249663e5128
unknown <crappy.naoto@gmail.com>
2019-07-21 01:42:57 +0900

    seederクラス　追記

--- database/seeds/small_questions.php
+++ database/seeds/small_questions.php
@@ -45,6 +45,16 @@ class small_questions extends Seeder
             ['genre_value'=>'beverb_verb','big_question_id'=>'3','question_num'=>'3','question'=>'私は飲まない。','answer'=>'I do not drink.',],
         ]);    
 
+        DB::table('genres')->insert([
+            ['genre'=>'Be動詞','genre_value'=>'beverb',],
+            ['genre'=>'一般動詞','genre_value'=>'verb',],
+            ['genre'=>'3人称単数','genre_value'=>'thirdperson',],
+            ['genre'=>'代名詞','genre_value'=>'pronoun',],
+            ['genre'=>'助動詞Can','genre_value'=>'can',],
+            ['genre'=>'現在進行形','genre_value'=>'presentgoing',],
+            ['genre'=>'Be動詞と一般動詞の使い分け','genre_value'=>'beverb_verb',],
+        ]); 
+
      
     }
 }
