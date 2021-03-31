d97a5ea953dd98cd3d3732fb436988b849eb09fa
NaotoSuzuki <crappy.naoto@gmail.com>
2021-03-31 20:11:56 +0900

    文法解説機能復活させた

--- resources/views/questions/afterQuestion.blade.php
+++ resources/views/questions/afterQuestion.blade.php
@@ -13,10 +13,11 @@ $user_id;
     <div class = "container , text-center">
     <h1>回答お疲れさまでした！</h1>
 
-    <h5><a href="/" class="header-menu">トップページに戻る</a></h5>
+    <h5><a href="/index" class="header-menu">トップページに戻る</a></h5>
     <h5><a href="{{ action('IndexController@show_Hists', $user_id) }}">回答履歴を見る</a></h5>
     <h5><a href="/questions/question/{{$genre_value}}" class="header-menu">同じ問題をやり直す</a></h5>
     </div>
+        <div id="lottie"></div>
 
-    <div id="lottie"></div>
+
 @endsection
