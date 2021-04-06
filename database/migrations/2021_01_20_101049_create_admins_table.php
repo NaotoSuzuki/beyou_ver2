e6aeaa88819b9a779f1a92a8340333ce34dbbbce
NaotoSuzuki <crappy.naoto@gmail.com>
2021-04-07 06:32:52 +0900

    データ型修正

--- database/migrations/2021_01_20_101049_create_admins_table.php
+++ database/migrations/2021_01_20_101049_create_admins_table.php
@@ -14,7 +14,7 @@ class CreateAdminsTable extends Migration
     public function up()
     {
         Schema::create('admins', function (Blueprint $table) {
-            $table->increment('id');
+            $table->bigIncrement('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
